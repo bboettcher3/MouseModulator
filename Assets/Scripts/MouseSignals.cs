09d7a7ff3648370bbd2cc8dda178cf4d2f983f2b
Brady Boettcher <bboettcher2@wisc.edu>
2022-06-28 11:54:06 +0200

    position signals and persisting angles

--- Assets/Scripts/MouseSignals.cs
+++ Assets/Scripts/MouseSignals.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class MouseSignals : MonoBehaviour
 {
   [HideInInspector]
+  public float mouseXPosition;
+  [HideInInspector]
+  public float mouseYPosition;
+  [HideInInspector]
   public float mouseXSpeed;
   [HideInInspector]
   public float mouseYSpeed;
@@ -21,11 +25,15 @@ public class MouseSignals : MonoBehaviour
   [HideInInspector]
   public float mouseScrollClick;
   [HideInInspector]
+  public float mouseScrollPosition;
+  [HideInInspector]
   public float mouseScrollSpeed;
 
   enum MouseSignal
   {
-    MOUSE_X_SPEED = 0,
+    MOUSE_X_POSITION = 0,
+    MOUSE_Y_POSITION,
+    MOUSE_X_SPEED,
     MOUSE_Y_SPEED,
     MOUSE_SPEED,
     MOUSE_ANGLE_DEG,
@@ -33,6 +41,7 @@ public class MouseSignals : MonoBehaviour
     MOUSE_LEFT_CLICK,
     MOUSE_RIGHT_CLICK,
     MOUSE_SCROLL_CLICK,
+    MOUSE_SCROLL_POSITION,
     MOUSE_SCROLL_SPEED,
     NUM_SIGNALS
   };
@@ -42,6 +51,7 @@ public class MouseSignals : MonoBehaviour
   private IntPtr[] sigs = new IntPtr[(int)MouseSignal.NUM_SIGNALS];
 
   private const int NUM_AVG_FRAMES = 20;
+  private const float POSITION_SPEED_SCALAR = 0.08f;
 
   private float[] mouseXSpeedBuffer = new float[NUM_AVG_FRAMES];
   private float[] mouseYSpeedBuffer = new float[NUM_AVG_FRAMES];
@@ -52,6 +62,8 @@ public class MouseSignals : MonoBehaviour
   void Start()
   {
     dev = mpr.mpr_dev_new("MouseModulator");
+    sigs[(int)MouseSignal.MOUSE_X_POSITION] = mpr.mpr_sig_new(dev, mpr.Direction.OUTGOING, "mouseXPosition", 1, mpr.Type.FLOAT, -1.0f, 1.0f);
+    sigs[(int)MouseSignal.MOUSE_Y_POSITION] = mpr.mpr_sig_new(dev, mpr.Direction.OUTGOING, "mouseYPosition", 1, mpr.Type.FLOAT, -1.0f, 1.0f);
     sigs[(int)MouseSignal.MOUSE_X_SPEED] = mpr.mpr_sig_new(dev, mpr.Direction.OUTGOING, "mouseXSpeed", 1, mpr.Type.FLOAT, -1.0f, 1.0f);
     sigs[(int)MouseSignal.MOUSE_Y_SPEED] = mpr.mpr_sig_new(dev, mpr.Direction.OUTGOING, "mouseYSpeed", 1, mpr.Type.FLOAT, -1.0f, 1.0f);
     sigs[(int)MouseSignal.MOUSE_SPEED] = mpr.mpr_sig_new(dev, mpr.Direction.OUTGOING, "mouseSpeed", 1, mpr.Type.FLOAT, 0.0f, 1.0f);
@@ -60,6 +72,7 @@ public class MouseSignals : MonoBehaviour
     sigs[(int)MouseSignal.MOUSE_LEFT_CLICK] = mpr.mpr_sig_new(dev, mpr.Direction.OUTGOING, "mouseLeftClick", 1, mpr.Type.INT, 0, 1);
     sigs[(int)MouseSignal.MOUSE_RIGHT_CLICK] = mpr.mpr_sig_new(dev, mpr.Direction.OUTGOING, "mouseRightClick", 1, mpr.Type.INT, 0, 1);
     sigs[(int)MouseSignal.MOUSE_SCROLL_CLICK] = mpr.mpr_sig_new(dev, mpr.Direction.OUTGOING, "mouseScrollClick", 1, mpr.Type.INT, 0, 1);
+    sigs[(int)MouseSignal.MOUSE_SCROLL_POSITION] = mpr.mpr_sig_new(dev, mpr.Direction.OUTGOING, "mouseScrollPosition", 1, mpr.Type.FLOAT, -1.0f, 1.0f);
     sigs[(int)MouseSignal.MOUSE_SCROLL_SPEED] = mpr.mpr_sig_new(dev, mpr.Direction.OUTGOING, "mouseScrollSpeed", 1, mpr.Type.FLOAT, -1.0f, 1.0f);
   }
 
@@ -83,7 +96,7 @@ public class MouseSignals : MonoBehaviour
     /* Get new mouse values */
     mouseXSpeedBuffer[curBufferIdx] = Input.GetAxis("Mouse X") / 4.0f;
     mouseYSpeedBuffer[curBufferIdx] = Input.GetAxis("Mouse Y") / 4.0f;
-    mouseScrollSpeedBuffer[curBufferIdx] = Input.mouseScrollDelta.y / 4.0f;
+    mouseScrollSpeedBuffer[curBufferIdx] = Input.mouseScrollDelta.y;
 
     /* Get moving averages of each signal */
     float mouseXSpeedSum = 0;
@@ -96,20 +109,36 @@ public class MouseSignals : MonoBehaviour
       mouseScrollSpeedSum += mouseScrollSpeedBuffer[i];
     }
 
-    /* Update libmapper signals */
+    /* Speed values */
     mouseXSpeed = mouseXSpeedSum / NUM_AVG_FRAMES;
     mouseYSpeed = mouseYSpeedSum / NUM_AVG_FRAMES;
     mouseSpeed = Mathf.Sqrt(Mathf.Pow(mouseXSpeed, 2) + Mathf.Pow(mouseYSpeed, 2));
-    mouseAngleRad = Mathf.Atan2(mouseYSpeed, mouseXSpeed);
-    if (mouseAngleRad < 0.0f)
+    mouseScrollSpeed = mouseScrollSpeedSum / NUM_AVG_FRAMES;
+
+    /* Position values */
+    mouseXPosition = Mathf.Clamp(mouseXPosition + (mouseXSpeed * POSITION_SPEED_SCALAR), -1.0f, 1.0f);
+    mouseYPosition = Mathf.Clamp(mouseYPosition + (mouseYSpeed * POSITION_SPEED_SCALAR), -1.0f, 1.0f);
+    mouseScrollPosition = Mathf.Clamp(mouseScrollPosition + (mouseScrollSpeed * POSITION_SPEED_SCALAR), -1.0f, 1.0f);
+
+    /* Angle values */
+    if (mouseSpeed > 0.05f)
     {
-      mouseAngleRad += 2 * Mathf.PI;
+      mouseAngleRad = Mathf.Atan2(mouseYSpeed, mouseXSpeed);
+      if (mouseAngleRad < 0.0f)
+      {
+        mouseAngleRad += 2 * Mathf.PI;
+      }
+      mouseAngleDeg = Mathf.Rad2Deg * mouseAngleRad;
     }
-    mouseAngleDeg = Mathf.Rad2Deg * mouseAngleRad;
+
+    /* Click values */
     mouseLeftClick = Input.GetMouseButton(0) ? 1 : 0;
     mouseRightClick = Input.GetMouseButton(1) ? 1 : 0;
     mouseScrollClick = Input.GetMouseButton(2) ? 1 : 0;
-    mouseScrollSpeed = mouseScrollSpeedSum / NUM_AVG_FRAMES;
+    
+    /* Update libmapper signal values */
+    mpr.mpr_sig_set_value(sigs[(int)MouseSignal.MOUSE_X_POSITION], mouseXPosition);
+    mpr.mpr_sig_set_value(sigs[(int)MouseSignal.MOUSE_Y_POSITION], mouseYPosition);
     mpr.mpr_sig_set_value(sigs[(int)MouseSignal.MOUSE_X_SPEED], mouseXSpeed);
     mpr.mpr_sig_set_value(sigs[(int)MouseSignal.MOUSE_Y_SPEED], mouseYSpeed);
     mpr.mpr_sig_set_value(sigs[(int)MouseSignal.MOUSE_SPEED], mouseSpeed);
@@ -118,9 +147,10 @@ public class MouseSignals : MonoBehaviour
     mpr.mpr_sig_set_value(sigs[(int)MouseSignal.MOUSE_LEFT_CLICK], mouseLeftClick);
     mpr.mpr_sig_set_value(sigs[(int)MouseSignal.MOUSE_RIGHT_CLICK], mouseRightClick);
     mpr.mpr_sig_set_value(sigs[(int)MouseSignal.MOUSE_SCROLL_CLICK], mouseScrollClick);
+    mpr.mpr_sig_set_value(sigs[(int)MouseSignal.MOUSE_SCROLL_POSITION], mouseScrollPosition);
     mpr.mpr_sig_set_value(sigs[(int)MouseSignal.MOUSE_SCROLL_SPEED], mouseScrollSpeed);
 
-    curBufferIdx = (curBufferIdx + 1) % NUM_AVG_FRAMES;
+    curBufferIdx = (curBufferIdx + 1) % NUM_AVG_FRAMES; // Increment moving average buffer index
   }
 
   void OnApplicationFocus(bool hasFocus)
